faaecc3f33d71097b7391b0fe86dba7a604c1f9d
hoonzinope <hoonzinope@gmail.com>
2025-08-03 17:43:10 +0900

    Refactor random number generation to include input validation for min and max values

--- util/randomNum.go
+++ util/randomNum.go
@@ -4,6 +4,13 @@ import (
 	"math/rand"
 )
 
+func init() {
+	rand.New(rand.NewSource(42)) // Seed the random number generator for reproducibility
+}
+
 func GenerateNumber(min int, max int) int {
+	if min >= max {
+		panic("min should be less than max")
+	}
 	return rand.Intn(max-min) + min // Generate a random integer between min and max
 }
